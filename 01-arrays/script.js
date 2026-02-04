902cd77cf89cf04b68d50ced2c8058e091035a02
odon-airoldi <odon.airoldi@gmail.com>
2026-02-04 15:52:15 +0100

    first commit

---
+++ 01-arrays/script.js
@@ -0,0 +1,53 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
+
+// 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = 'Patrick';
+console.log(teachers);
+
+// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+teachers.pop();
+const lastTeacher = teachers[teachers.length - 1];
+console.log(lastTeacher);
+
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+teachers.shift()
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+
+// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
+
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
+console.log(teachers);
+
+// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// e salvalo nella variabile lewisIndex
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(lewisIndex);
+
+// 8. Verifica se l'array teachers è vuoto
+// e salva il risultato nella variabile isTeachersEmpty
+
+if (teachers.lenght === 0) {
+  true_false = true
+} else {
+  true_false = false
+}
+const isTeachersEmpty = true_false;
+
+console.log(isTeachersEmpty)
