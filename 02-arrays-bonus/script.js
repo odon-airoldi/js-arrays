c3090ff66abcacc0918a21b16425bd323f879ffc
odon-airoldi <odon.airoldi@gmail.com>
2026-02-04 18:44:57 +0100

    solved exercise 02-arrays-bonus.3-4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -30,13 +30,14 @@ for (let i = 0; i < teachers.length; i++) {
 
 console.log(longNames)
 
-
 // 3. Rimuovi 'Ed' dall'array teachers
-
+teachers.splice(1, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio') !== -1;
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
